2f896292565a8c3ac0106fb96b0fb159a9aec3bc
madmaxer <madmaxer@85c254b7-193b-1943-a92e-57c90c425d62>
2009-11-07 20:45:29 +0000

    + In row selection mode selected rows are preserved after sorting, filters, tech   levels or reference system change.
    git-svn-id: svn://localhost@296 85c254b7-193b-1943-a92e-57c90c425d62

--- datalib/include/DblBufDGV.h
+++ datalib/include/DblBufDGV.h
@@ -22,6 +22,10 @@ public:
     {
         // Disable header size updates
         AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode::None;
+
+        // Store selected rows
+        StoreSelection();
+
         // Remove all data
         Rows->Clear();
     }
@@ -38,7 +42,45 @@ public:
 
         // Enable header size updates
         AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode::AllCells;
+
+        RestoreSelection();
     }
+
+protected:
+    void StoreSelection()
+    {
+        if( SelectedRows->Count > 0 )
+        {
+            m_StoredSelection = gcnew List<Object^>;
+            for each( DataGridViewRow ^r in SelectedRows )
+            {
+                Object ^o = r->Cells[0]->Value;
+                m_StoredSelection->Add( o );
+            }
+        }
+        else
+            m_StoredSelection = nullptr;
+    }
+
+    void RestoreSelection()
+    {
+        ClearSelection();
+        if( m_StoredSelection == nullptr )
+            return;
+
+        for each( DataGridViewRow ^row in Rows )
+        {
+            Object ^o = row->Cells[0]->Value;
+            for each( Object ^oSel in m_StoredSelection )
+                if( oSel == o )
+                {
+                    row->Selected = true;
+                    break;
+                }
+        }
+    }
+
+    List<Object^>^  m_StoredSelection;
 };
 
 }
